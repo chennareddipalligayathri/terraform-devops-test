13395f169d3affa1fe3c7b2d547821aa26b3c168
chennareddipalligayathri <77479095+chennareddipalligayathri@users.noreply.github.com>
2022-11-11 10:25:03 +0530

    syntax fixing

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ variable "image"{
 type = string
 default = "ami-0603cbe34fd08cb81"
   description = "Enter your APP Image to be deployed"
-  
+} 
 variable "hw" {
   type = string
   default = "t2.nano"
